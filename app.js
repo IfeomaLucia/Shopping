63fabf5eece00a04439cebc37dfb6ee3d671fdd2
IfeomaLucia <nwosuifeomalucia@gmail.com>
2018-07-31 12:38:03 +0100

    create the server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-const express = require('express');
+var express = require('express'); 
+var app = express();
+
+
+app.use(function(req, res, next){
+    res.status(200).json({
+        message: 'It works'
+    })
+})
+
+module.exports = app;
