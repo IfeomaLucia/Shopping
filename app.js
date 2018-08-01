4a0d275e5936082e89e4448d4eebae70378dac2e
IfeomaLucia <nwosuifeomalucia@gmail.com>
2018-08-01 16:47:56 +0100

    Add CORS

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 var express = require('express'); 
 var app = express();
 var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
 var productRouter = require('./api/routes/products');
 var orderRouter = require('./api/routes/orders');
 
 //Middleware function
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, GET, DELETE');
+        return res.status(200).json({});
+    }
+    next();
+})
 
 //Routes for requests
 app.use('/products', productRouter);
